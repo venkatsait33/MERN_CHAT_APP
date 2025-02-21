e8fe96f672e52a84dcc2b1737498af2f7cbb745b
venkatsait33 <129149218+venkatsait33@users.noreply.github.com>
2025-02-21 12:56:34 +0530

    cors

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios"
 import toast from "react-hot-toast"
 import { io } from "socket.io-client"
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000"
+const BASE_URL = import.meta.env.VITE_BACKEND_URL + "/api" || "https://mern-chat-app-backend-olive.vercel.app" + "/api"
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
