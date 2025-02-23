20e1f758e92ad27144798dceaaa17e4d169416d2
venkatsait33 <129149218+venkatsait33@users.noreply.github.com>
2025-02-23 11:45:15 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,18 @@ app.options("*", cors({
     credentials: true
 }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", URL);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use('/api/auth', userRoutes)
 app.use('/api/messages', messageRouter)
 
